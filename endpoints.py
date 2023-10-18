90a4c5fa427f423096bded7abedeb05d79c5ea5e
viktoriia <113999567+ViktoriiaVerych@users.noreply.github.com>
2023-10-19 00:17:01 +0300

    endpoints | tasks 28-35

--- endpoints.py
+++ endpoints.py
@@ -1,65 +1,91 @@
-from marshmallow import Schema, fields, validate
-from flask_sqlalchemy import SQLAlchemy
-from models import User, Prediction
-from flask_restful import reqparse, abort
-import config
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Blueprint
 from flask import Flask, request, jsonify
+import data_procession
 from datetime import datetime
-import data_procession as dp
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 
-class UserStatsSchema(Schema):
-    date = fields.Date(required=True)
-    userId = fields.Str(required=True)
-    tolerance = fields.Float(required=True)
+scheduler = BackgroundScheduler()
+scheduler.add_job(data_procession.fetch_and_update_data, 'interval', minutes=60)
+scheduler.start()
 
-arg_parser = UserStatsSchema()
-
-@app.route('/api/stats/user', methods=['GET'])
-def get_user_stats():
-    args = arg_parser.parse_args()
-    date_str = args['date']
-    user_id = args['userId']
-    date = datetime.fromisoformat(date_str)
-    user_data = User.query.filter_by(date=date, user_id=user_id).first()
+@app.route('/api/stats/user/online_time/<int:userId>', methods=['GET'])
+def get_online_time(userId):
+    user_data = data_procession.update_user_data(userId)
     
     if user_data is None:
-        abort(404, 'Invalid userId')
-    return jsonify(user_data)
+        return jsonify({'error': 'Invalid userId'}), 404
 
-@app.route('/api/predictions/users', methods=['GET'])
-def get_users_predictions():
-    args = arg_parser.parse_args()
-    date_str = args['date']
-    date = datetime.fromisoformat(date_str)
-    prediction = Prediction.query.filter_by(date=date).all()
+    online_time = data_procession.calculate_online_time(user_data)
+    return jsonify({'userId': userId, 'onlineTime': online_time})
+
+@app.route('/api/stats/user/average/<int:userId>', methods=['GET'])
+def get_average_times(userId):
+    user_data = data_procession.update_user_data(userId)
     
-    return jsonify(prediction)
+    if user_data is None:
+        return jsonify({'error': 'Invalid userId'}), 404
 
-@app.route('/api/predictions/user', methods=['GET'])
-def get_user_prediction():
-    args = arg_parser.parse_args()
-    date_str = args['date']
-    user_id = args['userId']
-    tolerance = args['tolerance']
-    date = datetime.fromisoformat(date_str)
-    prediction = Prediction.query.filter_by(date=date, user_id=user_id).first()
+    weekly_avg, daily_avg = data_procession.calculate_average_times(user_data)
+    return jsonify({'userId': userId, 'weeklyAverage': weekly_avg, 'dailyAverage': daily_avg})
+
+@app.route('/api/user/forget', methods=['POST'])
+def forget_user():
+    request_body = request.get_json()
+    userId = request_body.get('userId')
     
-    if prediction is None:
-        abort(404)
-    prediction['willBeOnline'] = prediction['onlineChance'] >= tolerance
-    return jsonify(prediction)
+    if data_procession.check_user_exists(userId):
+        data_procession.delete_user_data(userId)
+        return jsonify({'userId': userId})
+    else:
+        return jsonify({'error': 'User does not exist'}), 404
 
-@app.route('/api/stats/users', methods=['GET'])
-def get_users_stats():
-    args = arg_parser.parse_args()
-    date_str = args['date']
-    date = datetime.fromisoformat(date_str)
-    users_online = User.query.filter_by(date=date).count()
-    
-    return jsonify({'date': date_str, 'usersOnline': users_online})
+@app.errorhandler(404)
+def handle_invalid_user(error):
+    return jsonify({'error': 'Invalid userId'}), 404
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.errorhandler(500)
+def handle_server_error(error):
+    return jsonify({'error': 'Internal server error'}), 500
+
+stats_routes = Blueprint('stats_routes', __name__)
+
+@stats_routes.route('/api/stats/user/online_time/<int:userId>', methods=['GET'])
+def get_online_time(userId):
+    user_data = data_procession.update_user_data(userId)
+
+    if user_data is None:
+        return handle_invalid_user(404)
+
+    online_time = data_procession.calculate_online_time(user_data)
+    return jsonify({'userId': userId, 'onlineTime': online_time})
+
+@stats_routes.route('/api/stats/user/average/<int:userId>', methods=['GET'])
+def get_average_times(userId):
+    user_data = data_procession.update_user_data(userId)
+
+    if user_data is None:
+        return handle_invalid_user(404)
+
+    weekly_avg, daily_avg = data_procession.calculate_average_times(user_data)
+    return jsonify({'userId': userId, 'weeklyAverage': weekly_avg, 'dailyAverage': daily_avg})
+
+user_routes = Blueprint('user_routes', __name__)
+
+@user_routes.route('/api/user/forget', methods=['POST'])
+def forget_user():
+    request_body = request.get_json()
+    userId = request_body.get('userId')
+
+    if data_procession.check_user_exists(userId):
+        data_procession.delete_user_data(userId)
+        return jsonify({'userId': userId})
+    else:
+        return jsonify({'error': 'User does not exist'}), 404
+
+app.register_blueprint(stats_routes)
+app.register_blueprint(user_routes)
+
+if __name__ == "__main__":
+    app.run()
